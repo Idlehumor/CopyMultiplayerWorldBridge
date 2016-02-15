692f391af05f2387490652a3bed94143d0e23b47
Kris <cblair91@gmail.com>
2016-02-15 19:55:35 +0000

    Better if checking?

--- src/main/java/de/promolitor/copymultiplayerworldbridge/CopyMultiplayerWorldBridge.java
+++ src/main/java/de/promolitor/copymultiplayerworldbridge/CopyMultiplayerWorldBridge.java
@@ -132,10 +132,10 @@ public final class CopyMultiplayerWorldBridge extends JavaPlugin implements List
 			boolean isClaimed = true;
 			for(int[] cIds : chunkIds.get(player.getDisplayName())) {			
 				Claim claim = GriefPrevention.instance.dataStore.getClaimAt(new Location(player.getWorld(), cIds[0]*16, 0, cIds[1]*16), true, null);
-				if (claim == null || 
-						!(claim.getOwnerName().equals(player.getDisplayName()) || 
-						claim.getOwnerName().equals(player.getUniqueId().toString()) || 
-						claim.getOwnerName().equals(player.getDisplayName().toLowerCase()))) {
+				if (claim == null || (claim != null &&
+					!claim.getOwnerName().equals(player.getDisplayName()) && 
+					!claim.getOwnerName().equals(player.getUniqueId().toString()) && 
+					!claim.getOwnerName().equals(player.getDisplayName().toLowerCase()))) {
 					isClaimed = false;
 				}
 			}
